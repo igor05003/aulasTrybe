70210bbb715cf27b9cafc52bc867882fe8a40445
Igor Carvalho <igorr.ackerman@gmail.com>
2022-07-20 19:28:50 -0300

    Finish exercise about AND operator

---
+++ fundamentos/introducao_javaScript/4.1/script4.js
@@ -0,0 +1,16 @@
+const currentHour = 12;
+let message = "";
+
+if (currentHour >= 22) {
+  message = "Não deveríamos comer nada, é hora de dormir";
+}  else if (currentHour >= 18) {
+  message = "Rango da noite, vamos jantar :D";
+} else if (currentHour >= 14) {
+  message = "Vamos fazer um bolo pro café da tarde?"
+} else if (currentHour >= 11) {
+  message = "Hora do almoço!!";
+} else {
+  message = "Hmm, cheiro de café recém passado";    
+}
+
+console.log(message);
