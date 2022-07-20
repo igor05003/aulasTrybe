f1d2b00893b1ce6ca8f16503c8ef58ea681e55b6
Igor Carvalho <igorr.ackerman@gmail.com>
2022-07-20 19:03:21 -0300

    Finish exercises about loops

---
+++ fundamentos/introducao_javaScript/4.1/script3.js
@@ -0,0 +1,9 @@
+const rating = 80;
+
+if (rating >= 80) {
+  console.log("Parabéns, você foi aprovada(o)!")
+} else if (rating < 60) {
+  console.log("Você foi reprovada(o)")
+} else {
+  console.log("Você está na nossa lista de espera")
+}
