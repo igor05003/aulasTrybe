85bca20d840554f99b34616d5fcead2e00dfd4df
Igor Carvalho <igorr.ackerman@gmail.com>
2022-07-21 19:48:05 -0300

    Finish exercises of the day 4.2

--- fundamentos/introducao_javaScript/4.2/numbers.js
+++ fundamentos/introducao_javaScript/4.2/numbers.js
@@ -27,3 +27,63 @@ for (let index = 0; index < numbers.length; index += 1) {
 mediaAritmetica = mediaAritmetica / (numbers.length)
 
 console.log(mediaAritmetica);
+
+// 4.
+
+if (mediaAritmetica > 20) {
+    console.log("Valor maior que 20");
+} else {
+    console.log("Valor menor ou igual a 20");
+}
+
+// 5.
+let max = numbers[0];
+
+for (let index = 0; index < numbers.length; index += 1) {
+    let value = numbers[index];
+    if (value > max) {
+        max = value;
+    }
+}
+
+console.log("O valor máximo é: ", max)
+
+// 6.
+let imparVal = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    if ((numbers[index] % 2) !== 0 ) {
+        imparVal += 1;
+    } 
+}
+
+console.log("O número de valores ímpares é: ", imparVal);
+
+// 7.
+
+let minimum = numbers[0]
+
+for (let index = 0; index < numbers.length; index += 1) {
+    let value = numbers[index];
+    if (value < minimum) {
+        minimum = value;
+    }
+}
+
+console.log("O valor mínimo é: ", minimum);
+
+// 8.
+
+let array = [];
+
+for (let index = 1; index <= 25; index += 1) {
+    array.push(index);
+}
+
+console.log(array);
+
+// 9.
+
+for (let index = 0; index < array.length; index += 1) {
+    console.log(array[index] / 2);
+}
