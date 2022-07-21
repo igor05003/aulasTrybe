14c3fd797efe926f18dbe52e2314bfbebcce5672
Igor Carvalho <igorr.ackerman@gmail.com>
2022-07-20 21:06:56 -0300

    Finish biggerNumbers.js

---
+++ fundamentos/introducao_javaScript/4.1/biggerNumbers.js
@@ -0,0 +1,12 @@
+const a = 10;
+const b = 5;
+const c = 3;
+
+console.log("Qual é maior, a, b ou c?");
+if (a > b && a > c) {
+  console.log("a é maior.");
+} else if (b > a && b > c) {
+    console.log("b é maior.");
+} else if (c > a && c > b) {
+    console.log("c é maior.");
+}
