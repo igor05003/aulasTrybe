6716e3021f1710fa54d93c6c50071ba7a869fdd4
Igor Carvalho <igorr.ackerman@gmail.com>
2022-07-21 18:57:45 -0300

    Finish exercise 2 and 3

--- fundamentos/introducao_javaScript/4.2/numbers.js
+++ fundamentos/introducao_javaScript/4.2/numbers.js
@@ -1,5 +1,29 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+// 1.
+
 for (let index = 0; index < numbers.length; index += 1) {
-    console.log(numbers[index]);
+    console.log(index, "-->", numbers[index]);
 }
+
+// 2.
+
+let resultado = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    resultado += numbers[index];
+}
+
+console.log("Soma dos índices:", resultado);
+
+// 3.
+
+let mediaAritmetica = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    mediaAritmetica += (numbers[index]);
+}
+
+mediaAritmetica = mediaAritmetica / (numbers.length)
+
+console.log(mediaAritmetica);
