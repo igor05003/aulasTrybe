34489303cc38708ac8731ebe841530ece7e4a670
Igor Carvalho <igorr.ackerman@gmail.com>
2022-07-27 15:02:17 -0300

    finish exercises

--- fundamentos/introducao_javaScript/4.4/functions.js
+++ fundamentos/introducao_javaScript/4.4/functions.js
@@ -23,16 +23,7 @@ let myArray = [1, 5, 3, 6, 8, 2, 0];
 let array = [2, 8, 5, 3, 1, 0];
 
 function biggerNumber(array) {
-    let biggerIndex = 0;
-    let value = [0];
-
-    for (let index = 1; index < array.length; index += 1) {
-        if (array[index] > value) {
-            biggerIndex = index;
-        }
-    }
-    return biggerIndex;
+    
 }
 
 console.log(biggerNumber(myArray));
-console.log(biggerNumber(array));
